cd6c2c2ea29e22e49e739595432e2a7750713d51
Theo Petit <theo.p83@gmail.com>
2022-03-21 21:07:18 +0100

    web: script working

--- web/script.js
+++ web/script.js
@@ -1,6 +1,18 @@
-fetch('http://86.75.156.96/tmp.txt')
-    .then(response => response.text())
-    .then(data => {
-        // Do something with your data
-        console.log(data);
-    });
+async function fetchText() {
+    let response = await fetch('http://86.75.156.96/tmp.txt');
+    let data = await response.text();
+    return data;
+}
+
+var text;
+
+let data = fetchText();
+data.then(log);
+
+function log(data) {
+    // Assign data to html divs
+    var lines = data.split("\n");
+    lines = lines.slice(-10);
+    lines = lines.reverse();
+    console.log(lines);
+}
