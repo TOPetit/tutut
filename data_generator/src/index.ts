fd9723f59e1602c396a0a07fff6f99e8bb0d170f
Théo Petit <theo.p83@gmail.com>
2022-12-29 02:09:22 +0100

    tile: number of errors

--- data_generator/src/index.ts
+++ data_generator/src/index.ts
@@ -1,3 +1,5 @@
+import { number_of_errors } from "./Components/Tiles/number_of_errors";
 import { number_of_tutut } from "./Components/Tiles/number_of_tututs";
 
-console.log(number_of_tutut)
+console.log(number_of_tutut);
+console.log(number_of_errors);
