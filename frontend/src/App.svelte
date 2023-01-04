f249090c4e8861f0f0fcd544910f5a5d169efcd4
Théo Petit <theo.p83@gmail.com>
2023-01-04 20:20:58 +0100

    chart: pieChart init

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import BarChart from "./components/charts/bar/BarChart.svelte";
+  import PieChart from "./components/charts/pie/PieChart.svelte";
   let data: { user: string; color: string; data: number[] }[] = [
     { user: "Toto", color: "#cecece", data: [12, 3, 4, 6, 11, 0, 1] },
     { user: "Albert", color: "#ceaaaa", data: [2, 13, 14, 0, 11, 5, 2] },
@@ -14,6 +15,12 @@
     "06/01/2023",
     "07/01/2023",
   ];
+  let data_pie: { user: string; color: string; data: number }[] = [
+    { user: "Toto", color: "#cecece", data: 134 },
+    { user: "Albert", color: "#ceaaaa", data: 234 },
+    { user: "Walter White", color: "#aaaace", data: 16 },
+  ];
 </script>
 
 <BarChart width={700} height={400} {data} {labels} />
+<PieChart width={300} height={300} data={data_pie} />
---
+++ frontend/src/components/charts/pie/PieChart.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+    export let width: number;
+    export let height: number;
+    export let data: { user: string; color: string; data: number }[];
+
+    let total: number = data.reduce((acc, curr) => {
+        return acc + curr.data;
+    }, 0);
+
+    let size: number = Math.min(width, height) - 10;
+
+    let labels: string[] = data.map(
+        (element) => ((element.data * 100) / total).toFixed(2) + "%"
+    );
+
+    let selected_user: { user: string; color: string; data: number };
+</script>
+
+<svg {width} {height}>
+    {#each data as circle, index}
+        <path
+            cursor="pointer"
+            fill={circle.color}
+            opacity={selected_user ? (selected_user == circle ? 1 : 0.5) : 1}
+            stroke="black"
+            stroke-linejoin="round"
+            stroke-opacity={selected_user
+                ? selected_user == circle
+                    ? 1
+                    : 0.5
+                : 1}
+            d="M{width / 2},{height / 2}L{width / 2 + size / 2},{height /
+                2}A{size / 2},{size / 2},0,{circle.data / total > 0.5
+                ? 1
+                : 0},1,{width / 2 +
+                (size / 2) *
+                    Math.cos(2 * Math.PI * (circle.data / total))},{width / 2 +
+                (size / 2) * Math.sin(2 * Math.PI * (circle.data / total))}Z"
+            shape-rendering="auto"
+            style="stroke-linejoin: round; transform-origin: center; transform: rotate({index ==
+            0
+                ? 0
+                : data.slice(0, index).reduce((acc, curr) => {
+                      return acc + curr.data;
+                  }, 0) / total}turn);"
+            on:mouseover={() => {
+                selected_user = circle;
+            }}
+            on:focus={() => {
+                selected_user = circle;
+            }}
+            on:mouseout={() => {
+                selected_user = null;
+            }}
+            on:blur={() => {
+                selected_user = null;
+            }}
+        />
+        <text
+            x={width / 2 +
+                (size / 3.5) *
+                    Math.cos(
+                        2 *
+                            Math.PI *
+                            ((data.slice(0, index).reduce((acc, curr) => {
+                                return acc + curr.data;
+                            }, 0) +
+                                circle.data / 2) /
+                                total)
+                    )}
+            y={width / 2 +
+                (size / 3.5) *
+                    Math.sin(
+                        2 *
+                            Math.PI *
+                            ((data.slice(0, index).reduce((acc, curr) => {
+                                return acc + curr.data;
+                            }, 0) +
+                                circle.data / 2) /
+                                total)
+                    )}
+            pointer-events="none"
+            dy={5}
+            fill="black"
+            text-anchor="center">{labels[index]}</text
+        >
+    {/each}
+</svg>
