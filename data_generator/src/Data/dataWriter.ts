6ac8819db158e279ebf875655b6d7f03623d81e1
Théo Petit <theo.p83@gmail.com>
2023-01-09 18:15:09 +0100

    feat - user color: random color on generation

--- data_generator/src/Data/dataWriter.ts
+++ data_generator/src/Data/dataWriter.ts
@@ -4,9 +4,17 @@ import { Tile } from "../Components/components";
 
 export class Writer {
     private participants: string[] = [];
+    private colors: { [key: string]: string } = {};
     private tiles: Tile[] = [];
     public constructor(participants: string[]) {
         this.participants = participants;
+        participants.forEach((user) => {
+            this.colors[user] = this.getRandomHexColor()
+        })
+    }
+
+    private getRandomHexColor(): string {
+        return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
     }
 
     public addTile(tile: Tile | Tile[]): void {
