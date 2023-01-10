891664468416539f2d56f4b13888504cbc905f83
Théo Petit <theo.p83@gmail.com>
2023-01-10 17:44:44 +0100

    Changed labels (only 5 by 5)

--- data_generator/src/Components/Charts/BarCharts/Tutut_per_minute.ts
+++ data_generator/src/Components/Charts/BarCharts/Tutut_per_minute.ts
@@ -14,7 +14,7 @@ function compute(data: Data): { data: BarChartData, labels: string[] } {
     })
     let chartData: BarChartData = [];
     data.participants.forEach(participant => { chartData.push({ user: participant, data: tmp_ChartData[participant] }); })
-    return { data: chartData, labels: Array.from({ length: 60 }, (_, i) => i.toString().padStart(2, '0')) }
+    return { data: chartData, labels: Array.from({ length: 60 }, (_, i) => (i % 5 == 0) ? i.toString().padStart(2, '0') : "") }
 }
 
 export const chart_tutut_per_minute = new BarChart(ChartType.BAR, 'Fréquence des tututs dans la minute', compute, data);
