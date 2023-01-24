de720a2188124b7729c95a3bc1944b932976d2ee
Théo Petit <theo.p83@gmail.com>
2023-01-24 13:51:48 +0100

    try with basepath

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,9 +3,10 @@
   import Data from "./routes/data/Data.svelte";
   import Home from "./routes/Home.svelte";
   export let url: string = "";
+  export let basepath: string = "/tutut/";
 </script>
 
-<Router {url}>
+<Router {url} {basepath}>
   <Route path="data" component={Data} />
   <Route path="/" component={Home} />
 </Router>
