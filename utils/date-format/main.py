4bbfb7e390fa085ddf47cb995b5173ac408376f6
Théo Petit <theo.p83@gmail.com>
2023-01-25 21:42:44 +0100

    removing utils

--- utils/date-format/main.py
+++
@@ -1,8 +0,0 @@
-
-file_name = "voyage"
-
-with open(file_name + ".csv", 'r') as f:
-    data = f.read()
-
-with open(file_name + "_formatted" + ".csv", 'w') as f:
-    f.write(data.replace('/', '-'))
