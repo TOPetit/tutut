2ee35234f448caad9052f7852117281e830038d4
Théo Petit <theo.p83@gmail.com>
2023-01-25 19:27:28 +0100

    added bigdata

---
+++ frontend/src/routes/data/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import "../../style/font.css";
+    import complete_data from "../../../../data_generator/build/Data/complete_data.json";
+    import MessageContainer from "../../lib/data/MessageContainer.svelte";
+</script>
+
+<div class="data-page">
+    <MessageContainer messages={complete_data.messages} />
+</div>
+
+<style>
+    .data-page {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+</style>
