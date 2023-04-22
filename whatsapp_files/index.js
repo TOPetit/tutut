7b673e0b18543f62dfaa1689e85023e8f714327b
Théo Petit <theo.p83@gmail.com>
2023-04-22 18:18:51 +0200

    hoho

--- whatsapp_files/index.js
+++ whatsapp_files/index.js
@@ -45,7 +45,7 @@ async function run(subject) {
     logger.info(`email: ${email.date}`);
     for (const file of email.files) {
         const lines = Buffer.from(file.buffer).toString().split('\n');
-        fs.writeFileSync(resolve(__dirname, 'downloads/raw_data.txt'), lines.join('\n'), 'utf8', (err) => {
+        fs.writeFileSync('downloads/raw_data.txt', lines.join('\n'), 'utf8', (err) => {
             if (err) {
                 console.error(err);
                 return;
